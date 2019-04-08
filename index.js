ec7c3937250e6edea71d9ab49300daeb18e43c30
gabriell <gabriell@CE460>
2019-04-07 22:29:12 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// biblioteca js que faz o mapeamento das pastas em função do server.js
+require('rootpath')(); 
+// Inicialização do express. 
+var express = require('express');
+// bibloteca que ajuda no parse de mensagens requisitadas que contém JSON
+var bodyParser = require('body-parser');
+
+// carrega as configurações mapeadas no json
+var config = require('config.json');
+
+// Criação da API e indicação que trabalha com JSON
+var api = express();
+api.use(bodyParser.urlencoded({ extended: false }));
+api.use(bodyParser.json());
+api.use('/api/pessoas', require('./controllers/api/pessoas.controller'));
+
+// Porta fixa
+var apiPort = 6000;
+
+
+// start server API
+var serverAPI = api.listen(apiPort, function () {
+    console.log('Server API listening at http://' + serverAPI.address().address + ':' + serverAPI.address().port);
+});
---
+++ services/pessoa.service.js
@@ -0,0 +1,131 @@
+var config = require('config.json');
+var Q = require('q');
+var mongo = require('mongoskin');
+var db = mongo.db(config.connectionString, { native_parser: true });
+db.bind('people');
+
+var service = {};
+service.create = create;
+service.getById = getById;
+service.listPeople = listPeople;
+service.update = update;
+service.delete = _delete;
+
+module.exports = service;
+
+
+function create(pessoaParam) {
+    var deferred = Q.defer();
+
+    // validation
+    db.people.findOne(
+        { personName: pessoaParam.personName },
+        function (err, person) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            if (person) {
+                // username already exists
+                deferred.reject('PersonName "' + pessoaParam.personName + '" is already taken');
+            } else {
+                createPerson();
+            }
+        });
+
+    function createPerson() {
+        db.people.insert(
+            pessoaParam,
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function getById(_id) {
+    var deferred = Q.defer();
+
+    db.people.findById(_id, function (err, person) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (person) {
+            // return user (without hashed password)
+            deferred.resolve(person);
+        } else {
+            // user not found
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+
+function listPeople() {
+    var deferred = Q.defer();
+
+    db.people.find().toArray(function (err, people) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (people) {
+            // return user (without hashed password)
+            deferred.resolve(people);
+        } else {
+            // user not found
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+
+function update(personParam) {
+    var deferred = Q.defer();
+
+    // validation
+    db.people.findById(personParam.personId, function (err, person) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (person) {
+            updatePerson();
+        }
+    });
+
+    function updatePerson() {
+        // fields to update
+        var set = {
+            personName: personParam.personName,
+            personDateOfBirth: personParam.personDateOfBirth,
+            personPlaceOfBirth: personParam.personPlaceOfBirth,
+        };
+
+        db.people.update(
+            { _id: mongo.helper.toObjectID(personParam.personId) },
+            { $set: set },
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function _delete(_id) {
+    var deferred = Q.defer();
+
+    db.people.remove(
+        { _id: mongo.helper.toObjectID(_id) },
+        function (err) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            deferred.resolve();
+        });
+
+    return deferred.promise;
+}
+
