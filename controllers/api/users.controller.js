e9003ea87b7c5199c4eb4e55cf7fd4beef1a731a
gabriell <gabriell@CE460>
2019-04-20 17:54:43 -0300

    Autenticação JWt

---
+++ controllers/api/users.controller.js
@@ -0,0 +1,86 @@
+﻿// Esse é o mesmo controller do Mongo Stack! Apenas fiz a tradução das mensagens
+
+var express = require('express');
+var router = express.Router();
+var userService = require('services/user.service');
+
+// routes
+router.post('/authenticate', authenticateUser);
+router.post('/register', registerUser);
+router.get('/current', getCurrentUser);
+router.put('/:_id', updateUser);
+router.delete('/:_id', deleteUser);
+
+module.exports = router;
+
+function authenticateUser(req, res) {
+    userService.authenticate(req.body.username, req.body.password)
+        .then(function (response) {
+            if (response) {
+                // authentication successful
+                res.send({ userId: response.userId, token: response.token });
+            } else {
+                // authentication failed
+                res.status(401).send('Usuário e/ou senha inválidos');
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function registerUser(req, res) {
+    userService.create(req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getCurrentUser(req, res) {
+    userService.getById(req.session.userId)
+        .then(function (user) {
+            if (user) {
+                res.send(user);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function updateUser(req, res) {
+    var userId = req.session.userId;
+    if (req.params._id !== userId) {
+        // can only update own account
+        return res.status(401).send('Você só pode alterar a sua própria conta');
+    }
+
+    userService.update(userId, req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function deleteUser(req, res) {
+    var userId = req.param.id;
+    if (req.params._id !== userId) {
+        // can only delete own account
+        return res.status(401).send('Você só pode excluir a sua própria conta');
+    }
+
+    userService.delete(userId)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
--- server.js
+++ server.js
@@ -4,17 +4,30 @@ require('rootpath')();
 var express = require('express');
 // bibloteca que ajuda no parse de mensagens requisitadas que contém JSON
 var bodyParser = require('body-parser');
+// essa biblioteca será utilizada na API para fazer autenticaçao seguindo o método JWT. 
+// Se quiser estudar um pouco mais sobre JWT, pesquise aqui
+// https://jwt.io/introduction/
+var expressJwt = require('express-jwt');
+var config = require("./config.json");
 
 // Criação da API e indicação que trabalha com JSON
 var api = express();
 api.use(bodyParser.urlencoded({ extended: false }));
 api.use(bodyParser.json());
+
+
+// Essa configuração na API indica que haverá JWT para cada endpoint / rota método, com exceção dos métodos
+// de autenticação, registro de usuários e sobre. Essa camada de segurança é muito boa, porque ajuda
+// na diminuição do tratamento de mensagens indevidas na aplicação
+api.use('/api', expressJwt({ secret: process.env.secret || config.secret }).unless({ path: ['/api/about','/api/users/authenticate', '/api/users/register'] }));
+
+
 api.use('/api/pessoas', require('./controllers/api/pessoas.controller'));
 api.use('/api/about', require('./controllers/api/about.controller'));
-
+api.use('/api/users', require('./controllers/api/users.controller'));
 
 // process.env.PORT é uma variável injetada pelo Azure Web App. Caso ela não exista, será utilizada a porta fixa (6000)
-var apiPort = process.env.PORT || 6000;
+var apiPort = process.env.PORT || config.port;
 
 
 // start server API
