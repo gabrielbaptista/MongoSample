5304ff98d6b0c97eaf0acb52c14532e4fd3078bb
gabriell <gabriell@CE460>
2019-04-21 17:32:24 -0300

    Configuração da connectionString em Nuvem

--- services/user.service.js
+++ services/user.service.js
@@ -4,7 +4,8 @@ var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var Q = require('q');
 var mongo = require('mongoskin');
-var db = mongo.db(config.connectionString, { native_parser: true });
+var connection = process.env.connectionString || config.connectionString;
+var db = mongo.db(connection, { native_parser: true });
 db.bind('users');
 
 var service = {};
